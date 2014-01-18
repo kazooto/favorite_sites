79dc9eda1bd40cfb10545d9d3e4d57f146e395a6
Kazuto Kawamura <k.kazooto@gmail.com>
2014-01-18 21:44:10 +0900

    initialize rails application

---
+++ favorite_sites/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FavoriteSites::Application.config.secret_key_base = 'bca9a9b3f81f81ae8f6a549a0eb9ae7011441587c1a9c6014a266212525676a233381c1837cc1f687483a0c0e9315640907e2f6ba4cdc90a4eb39b9a83d52ade'
---
+++ favorite_sites/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FavoriteSites::Application.config.session_store :cookie_store, key: '_favorite_sites_session'
